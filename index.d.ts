c1f10b9d7c7c6a70d0cb07faf878209ca65b2490
mung3477 <mung3477@naver.com>
2022-07-12 14:29:43 +0900

    settings: SVG를 컴포넌트처럼 사용할 수 있게 설정

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+
+declare module '*.svg' {
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
--- next.config.js
+++ next.config.js
@@ -5,4 +5,31 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: { removeViewBox: false },
+                  },
+                },
+              ],
+            },
+            titleProp: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };

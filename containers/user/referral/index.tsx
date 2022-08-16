2b01913d5f909d2e58b9d4fc887cbc0b1e53ba8d
mung3477 <mung3477@naver.com>
2022-08-16 22:01:00 +0900

    chore: Tokens 페이지 삭제, referral 폴더 이름 디자인과 일치화

--- containers/user/referral/index.tsx
+++
@@ -1,9 +0,0 @@
-import { BasicWrapper, SectionWrapper } from 'components/global';
-
-export default function Referral() {
-  return (
-    <BasicWrapper>
-      <SectionWrapper header="Referral" marginBottom="20px" />
-    </BasicWrapper>
-  );
-}
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,8 +1,8 @@
 import { ClientRoute } from 'hooks/useSPARouting';
 /* @todo: lazy import가 가능하지 않을까? */
 import Account from 'containers/user/account';
-import Tokens from 'containers/user/tokens';
 import Logs from 'containers/user/points';
+import Referrals from 'containers/user/referrals';
 import Calendar from 'containers/user/calendar';
 import Staking from 'containers/user/staking';
 
@@ -10,8 +10,8 @@ export default function UserPage() {
   return (
     <>
       <ClientRoute path="/user" component={<Account />} />
-      <ClientRoute path="/user/tokens" component={<Tokens />} />
       <ClientRoute path="/user/points" component={<Logs />} />
+      <ClientRoute path="/user/referrals" component={<Referrals />} />
       <ClientRoute path="/user/calendar" component={<Calendar />} />
       <ClientRoute path="/user/staking" component={<Staking />} />
     </>

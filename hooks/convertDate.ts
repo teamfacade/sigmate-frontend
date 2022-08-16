510b251df0956238821fda4440cfadae4806af66
mung3477 <mung3477@naver.com>
2022-08-16 17:12:55 +0900

    feat(DesignFix): time 표기법 변경

--- hooks/convertDate.ts
+++ hooks/convertDate.ts
@@ -1,9 +1,30 @@
-export default function convertDate(date: Date) {
+const logFormatter = new Intl.DateTimeFormat('en', {
+  month: 'short',
+  day: '2-digit',
+  year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  timeZoneName: 'short',
+  hourCycle: 'h24',
+});
+
+const regex = /,*\s/g;
+
+export default function convertDate(
+  event: Date,
+  format: string,
+  delimeter: string | undefined
+) {
   let converted = '';
 
-  converted += `${date.getMonth()  }.`;
-  converted += `${date.getDate()  }.`;
-  converted += date.getFullYear();
+  switch (format) {
+    case 'log':
+      converted = logFormatter.format(event);
+      break;
+    default:
+      break;
+  }
 
+  converted = converted.replaceAll(regex, delimeter || ' ');
   return converted;
 }

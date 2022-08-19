b0c4b566ed932f36dfeb722008ede7d1ae5fddd8
mung3477 <mung3477@naver.com>
2022-08-19 16:33:35 +0900

    refactor: wiki component를 edit과 read로 구분

--- components/main/wiki/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as EditBlock } from './EditBlock';
-export { default as Paragraph } from './Paragraph';
-export { default as Heading } from './Heading';
-export { default as Title } from './Title';

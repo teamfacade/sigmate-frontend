3327136a6d42e65c9735ed0a8125fe2671ea22ab
mung3477 <mung3477@naver.com>
2022-09-09 00:34:09 +0900

    fix: 또 api/v1 빼먹었어요

--- lib/global/axiosInstance.ts
+++ lib/global/axiosInstance.ts
@@ -9,7 +9,7 @@ type RenewResultType = {
 };
 
 const Axios = axios.create({
-  baseURL: 'http://api.sigmate.io:5100',
+  baseURL: 'http://api.sigmate.io:5100/api/v1',
 });
 
 let renewingAccess = false;

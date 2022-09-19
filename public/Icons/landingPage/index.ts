d8d4c8259cf5534150cc9aefe77c8b1c73dca0d5
mung3477 <mung3477@naver.com>
2022-09-19 22:33:13 +0900

    fix: 구 메인페이지 삭제

--- public/Icons/landingPage/index.ts
+++ public/Icons/landingPage/index.ts
@@ -1,15 +1,3 @@
-export { default as GreenClick } from './GreenClick.svg';
-export { default as YellowTriangle } from './YellowTriangle.svg';
-export { default as Star } from './Star.svg';
-
-export { default as Apple } from './Apple.png';
-export { default as Mango } from './Mango.png';
-export { default as Lime } from './Lime.png';
-export { default as UltraViolet } from './UltraViolet.png';
-export { default as Orca } from './Orca.png';
-export { default as MangoSteen } from './MangoSteen.png';
-export { default as Strawberry } from './Strawberry.png';
-
 export { default as SigmateLogo } from './SigmateLogo.svg';
 export { default as WavingHand } from './WavingHand.svg';
 

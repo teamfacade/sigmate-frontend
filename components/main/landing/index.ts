6b637ad6d47a1aebc9790b2ed27464de3916774e
mung3477 <mung3477@naver.com>
2022-09-19 22:36:03 +0900

    Delete prev landing page's components

--- components/main/landing/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Member } from './Member';
--- containers/main/landing/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Members } from './Members';
-export { default as Phrases } from './Phrases';
-
-export { default as CatchPhrase } from './CatchPhrase';
-export { default as Welcomes } from './Welcomes';

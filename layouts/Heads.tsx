0af54b1ceab8632b6db129fb421011c60b901470
mung3477 <mung3477@naver.com>
2022-09-27 17:55:37 +0900

    feat(Dev): OpenGraph 메타데이터 추가

--- layouts/Heads.tsx
+++ layouts/Heads.tsx
@@ -1,11 +1,23 @@
 import Head from 'next/head';
 import Script from 'next/script';
 
-export default function Heads() {
+type PropsType = {
+  curPath: string;
+};
+
+export default function Heads({ curPath }: PropsType) {
   return (
     <>
       <Head>
         <title>Sigmate</title>
+        <meta property="og:url" content={curPath} />
+        <meta property="og:title" content="Sigmate" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/Icons/Favicon/Logo_310x310.png" />
+        <meta
+          property="og:description"
+          content="All you need to know about your NFT"
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1"
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   if (router.pathname.startsWith('/auth')) {
     return (
       <>
-        <Heads />
+        <Heads curPath={router.pathname} />
         {getLayout(<Component {...pageProps} />)}
       </>
     );
@@ -43,7 +43,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   if (router.pathname.startsWith('/main')) {
     return (
       <>
-        <Heads />
+        <Heads curPath={router.pathname} />
         <Navbar />
         <MainLayout
           needsWrapper={
@@ -64,7 +64,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   if (router.pathname.startsWith('/user')) {
     return (
       <>
-        <Heads />
+        <Heads curPath={router.pathname} />
         <Navbar />
         <UserLayout>{getLayout(<Component {...pageProps} />)}</UserLayout>
         <Footer />
@@ -74,7 +74,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   if (router.pathname.startsWith('/admin')) {
     return (
       <>
-        <Heads />
+        <Heads curPath={router.pathname} />
         <Navbar />
         <AdminLayout>{getLayout(<Component {...pageProps} />)}</AdminLayout>
         <Footer />
@@ -83,7 +83,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   }
   return (
     <>
-      <Heads />
+      <Heads curPath={router.pathname} />
       {getLayout(<Component {...pageProps} />)}
       <Footer />
     </>

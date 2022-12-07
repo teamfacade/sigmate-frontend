ed08559028a87d896f3f0c9d9f3a89345b332949
Woosung Joung <mung3477@naver.com>
2022-12-07 14:48:03 +0900

    fix: upcoming 페이지 막음

--- components/Navbar/Links.tsx
+++ components/Navbar/Links.tsx
@@ -21,11 +21,11 @@ export default memo(function Links({ onClickShowMenu }: PropsType) {
           <a onClick={onClickShowMenu}>Forum</a>
         </Link>
       </li>
-      <li>
+      {/* <li>
         <Link href="/main/upcoming">
           <a onClick={onClickShowMenu}>Upcoming</a>
         </Link>
-      </li>
+      </li> */}
       <li>
         <Link href="/main/leaderboard">
           <a onClick={onClickShowMenu}>Leaderboard</a>

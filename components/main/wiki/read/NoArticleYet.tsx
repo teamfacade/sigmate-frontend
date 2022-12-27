108f9f49953a5d6cb71437d6c44f65eca823608f
Woosung Joung <mung3477@naver.com>
2022-12-27 19:36:46 +0900

    feat(ExtraUI): 검색 결과 UI 수정

--- components/main/wiki/read/NoArticleYet.tsx
+++ components/main/wiki/read/NoArticleYet.tsx
@@ -22,64 +22,24 @@ export default function NoArticleYet({ title }: PropsType) {
   }, [signedIn, userName]);
 
   return (
-    <Wrapper>
-      <h1>{title}</h1>
-      <FlexWrapper>
-        <p>Can&apos;t find what you are looking for?</p>
-        <button type="button" onClick={onClick}>
-          <p>Create new article</p>
-        </button>
-      </FlexWrapper>
-    </Wrapper>
+    <Button type="button" onClick={onClick}>
+      <p>Create new article</p>
+    </Button>
   );
 }
 
-const Wrapper = styled.div`
-  h1 {
-    margin: 0 0 18px 0;
-    color: ${styles.colors.logoColor};
-    font-size: 40px;
-    font-weight: bold;
-    line-height: 110%;
-    text-align: left;
-  }
-`;
-
-const FlexWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  @media (max-width: 728px) {
-    flex-direction: column;
-    align-items: flex-start;
-
-    button {
-      margin-top: 20px !important;
-    }
-  }
+const Button = styled.button`
+  height: 40px;
+  border: none;
+  border-radius: 8px;
+  background-color: ${styles.colors.emphColor};
+  font-size: 18px;
+  cursor: pointer;
+  text-align: center;
 
   p {
-    margin: 0 16px 0 0;
-    color: ${styles.colors.logColor};
-    font-size: 18px;
-  }
-
-  button {
-    flex: 0 0 auto;
-    width: 200px;
-    height: 40px;
-    border: none;
-    border-radius: 8px;
-    background-color: ${styles.colors.emphColor};
-    font-size: 18px;
-    cursor: pointer;
-    text-align: center;
-
-    p {
-      margin: 0;
-      color: #ffffff;
-      white-space: nowrap;
-    }
+    margin: 0;
+    color: #ffffff;
+    white-space: nowrap;
   }
 `;

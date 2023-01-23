8cc5bc15fdf07ce2182a2083d53c57851f18fbd9
Woosung Joung <mung3477@naver.com>
2023-01-23 17:32:51 +0900

    feat(NewLanding): 이미지 파일들 수정

--- public/Icons/landingPage/index.ts
+++ public/Icons/landingPage/index.ts
@@ -1,12 +1,15 @@
 export { default as SigmateLogo } from './SigmateLogo.svg';
 export { default as ReadingGlass } from './ReadingGlass.svg';
+export { default as DiscordBlack } from './DiscordBlack.svg';
+export { default as TwitterBlack } from './TwitterBlack.svg';
+export { default as TelegramBlack } from './TelegramBlack.svg';
+export { default as MediumBlack } from './MediumBlack.svg';
 export { default as Correlation } from './Correlation.svg';
 export { default as BookIcon } from './BookIcon.svg';
 export { default as CalendarIcon } from './CalendarIcon.svg';
 export { default as UpcomingExample } from './UpcomingExample.png';
-export { default as UpcomingDetail } from './UpcomingDetail.png';
 export { default as ForumIcon } from './ForumIcon.svg';
-export { default as ForumCaptureImg } from './ForumCaptureImg.png';
+export { default as ForumExample } from './ForumExample.png';
 
 export { default as CorrelationEx } from './CorrelationEx.png';
 export { default as Roadmap } from './Roadmap.png';
